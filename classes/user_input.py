69c979f85366d9fdbf525b5c11b525038b874cf9
Manicarus <swkang7229@gmail.com>
2021-10-13 02:38:58 +0900

    defined additional options for executable   1. tags   2. source   3. safe flag   4. remove flag   5. dry-run flag
    removed unused configuration variables
      - upload_dir
      - tags
    
    initialized config file path and failsafe directory path with default
    values using appdirs package
    
    renamed few variables

--- classes/user_input.py
+++ classes/user_input.py
@@ -3,61 +3,111 @@ import configparser
 import appdirs
 import os
 import shutil
+import pprint
+import dryable
 from distutils.util import strtobool
+
+APPNAME = 'szuru-toolkit'
 
 class UserInput:
     def __init__(self):
-        self.booru_address   = ''
-        self.booru_api_token = ''
-        self.booru_api_url   = ''
-        self.booru_offline   = ''
-        self.booru_headers   = ''
-        self.sankaku_url     = ''
-        self.query           = ''
-        self.preferred_booru = ''
-        self.fallback_booru  = ''
-        self.upload_dir      = ''
-        self.tags            = []
-        self.local_temp_path = ''
+        self.config_file_path  = ''
+        self.failsafe_dir_path = ''
+        self.local_temp_path   = ''
+        self.booru_address     = ''
+        self.booru_api_token   = ''
+        self.booru_api_url     = ''
+        self.booru_offline     = ''
+        self.booru_headers     = ''
+        self.sankaku_url       = ''
+        self.query             = ''
+        self.preferred_booru   = ''
+        self.fallback_booru    = ''
+        self.rating            = ''
+        self.tags              = []
+        self.source            = []
+
+        # Default values
+        config_file = 'config.ini'
+        config_dir = appdirs.user_config_dir(APPNAME, appauthor=False)
+        config_file_path = os.path.join(config_dir, config_file)
+
+        failsafe_dir = 'failsafe'
+        data_dir = appdirs.user_data_dir(APPNAME, appauthor=False)
+        failsafe_dir_path = os.path.join(data_dir, failsafe_dir)
+
+        self.config_file_path = config_file_path
+        self.failsafe_dir_path = failsafe_dir_path
 
     def parse_input(self):
         """
         Parse the user input to the script auto_tagger.py and set the object attributes accordingly.
         """
 
+        # Default values
+        default_source_path = os.getcwd()
+
         # Create the parser
         parser = argparse.ArgumentParser(description='This script will automagically tag your szurubooru posts based on your input query.')
 
         # Add the arguments
-        parser.add_argument('--sankaku_url', dest='sankaku_url', help='Fetch tags from specified Sankaku URL instead of searching IQDB.')
-        parser.add_argument('query', help='Specify a single post id to tag or a szuru query. E.g. \'date:today tag-count:0\'')
+        parser.add_argument(
+            '--sankaku_url', dest='sankaku_url',
+            help='Fetch tags from specified Sankaku URL instead of searching IQDB.'
+        )
+        parser.add_argument(
+            'query', nargs='?',
+            help='Specify a single post id to tag or a szuru query. E.g. \'date:today tag-count:0\''
+        )
+        parser.add_argument(
+            '-s', '--source', nargs='*',
+            default=[default_source_path],
+            help='List of source directory or filename (default=<current working directory>)'
+        )
+        parser.add_argument(
+            '-t', '--tags', nargs='*',
+            default=[],
+            help='Upload images with provided tags'
+        )
+        parser.add_argument(
+            '--safe',
+            action='store_true',
+            help='Upload images with safe flag, otherwise images are marked unsafe'
+        )
+        parser.add_argument(
+            '--remove',
+            action='store_true',
+            help='Remove images after upload is complete. Ignored when dry-run'
+        )
+        parser.add_argument(
+            '--dry-run',
+            action='store_true',
+            help='Execute without creating/removing anything'
+        )
 
         # Execute the parse_args() method
         args = parser.parse_args()
 
         self.sankaku_url = args.sankaku_url
         self.query       = args.query
+        self.source      = args.source
+        self.remove      = args.remove
+        self.rating      = 'safe' if args.safe else 'unsafe'
+        self.tags        = args.tags
+
+        # Activate/deactivate dryable decorator
+        dryable.set(args.dry_run)
 
     def parse_config(self):
         """
         Parse the user config and set the object attributes accordingly.
         """
-        
-        appname = 'szuru-toolkit'
-        config_file = 'config.ini'
-        
-        try:
-            virtual_env = os.environ['VIRTUAL_ENV']
-            config_dir = os.path.join(virtual_env, 'config', appname)
-        except:
-            config_dir = appdirs.user_config_dir(appname)
-        config_path = os.path.join(config_dir, config_file)
-        
-        if not os.path.isfile(config_path):
-            print('Could not find configuration file:', config_path)
-        
+
+        if not os.path.isfile(self.config_file_path):
+            print('Could not find configuration file:', self.config_file_path)
+
         config = configparser.ConfigParser()
-        config.read(config_path)
+        config.read(self.config_file_path)
 
         self.booru_address   = config['szurubooru']['address']
         self.booru_api_url   = self.booru_address + '/api'
@@ -66,9 +116,11 @@ class UserInput:
         self.booru_offline   = strtobool(config['szurubooru']['offline'])
         self.preferred_booru = config['options'].get('preferred_booru', 'danbooru')
         self.fallback_booru  = config['options'].get('fallback_booru', 'sankaku')
-        self.upload_dir      = config['options']['upload_dir']
-        self.tags            = config['options']['tags'].split(',')
         self.local_temp_path = config['options'].get('local_temp_path', 'tmp')
+
+        config_failsafe_dir = config['options']['failsafe_directory']
+        if config_failsafe_dir:
+            self.failsafe_dir_path = config_failsafe_dir
 
     def describe(self):
         """
@@ -76,15 +128,18 @@ class UserInput:
         """
 
         data = {
-            'booru_address': self.booru_address,
-            'booru_api_url': self.booru_api_url,
-            'booru_api_token': self.booru_api_token,
-            'booru_headers': self.booru_headers,
-            'booru_offline': self.booru_offline,
-            'preferred_booru': self.preferred_booru,
-            'fallback_booru': self.fallback_booru,
-            'upload_dir': self.upload_dir,
-            'tags': self.tags,
+            'booru_address'   : self.booru_address,
+            'booru_api_url'   : self.booru_api_url,
+            'booru_api_token' : self.booru_api_token,
+            'booru_headers'   : self.booru_headers,
+            'booru_offline'   : self.booru_offline,
+            'preferred_booru' : self.preferred_booru,
+            'fallback_booru'  : self.fallback_booru,
+            'tags'            : self.tags,
+            'source'          : self.source,
+            'failsafe_dir_path'   : self.failsafe_dir_path,
         }
 
-        print(data)
+        print()
+        print('Configuration :')
+        pprint.PrettyPrinter().pprint(data)
